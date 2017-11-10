ad4713bda02497ffce859a35249f38e4b46d4591
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-11-10 01:41:58 +0100

    Write to file

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -1,5 +1,9 @@
 package lab4;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
     public static void main(String[] args) {
         Document cv = new Document("Jan Kowalski - CV");
@@ -20,6 +24,14 @@ public class Main {
                     .addListItem("Java")
             );
 
+        // Write to file
+        try {
+            cv.writeHTML(new PrintStream("cv.html", "UTF-8"));
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // As well as to the console
         cv.writeHTML(System.out);
     }
 }
