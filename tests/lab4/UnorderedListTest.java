493a0128efe88f947bfee77100e17d47404c3b47
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-11-12 23:08:14 +0100

    Added test for unordered list

---
+++ tests/lab4/UnorderedListTest.java
@@ -0,0 +1,33 @@
+package lab4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnorderedListTest extends HtmlTagTest {
+    @Test
+    void test_list_is_empty_when_there_are_no_items() {
+        UnorderedList list = new UnorderedList();
+
+        String output = this.output(list);
+
+        assertTrue(output.contains("<ul"));
+        assertTrue(this.getStringBetweenTags(output).isEmpty());
+        assertTrue(output.contains("</ul>"));
+    }
+
+    @Test
+    void test_list_contains_added_items() {
+        UnorderedList list = new UnorderedList();
+        list.addItem(new ListItem("item 1"));
+        list.addItem(new ListItem("item 2"));
+
+        String output = this.output(list);
+
+        assertTrue(output.contains("<ul"));
+        assertTrue(output.contains("<li"));
+        assertTrue(output.contains(">item 1</li>"));
+        assertTrue(output.contains(">item 2</li>"));
+        assertTrue(output.contains("</ul>"));
+    }
+}
