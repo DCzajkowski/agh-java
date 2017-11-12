e69c1913074418d5e1e26cd197856b31c515ec4d
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-11-12 22:51:18 +0100

    Added paragraph test

---
+++ tests/lab4/HtmlTagTest.java
@@ -0,0 +1,26 @@
+package lab4;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public abstract class HtmlTagTest {
+    protected String output(HtmlTag tag) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outputStream);
+
+        tag.writeHTML(out);
+
+        String output = "";
+
+        try {
+            output = outputStream.toString("UTF8");
+        } catch (UnsupportedEncodingException e) {
+            fail("Unsupported charset");
+        }
+
+        return output;
+    }
+}
---
+++ tests/lab4/ParagraphTest.java
@@ -0,0 +1,39 @@
+package lab4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParagraphTest extends HtmlTagTest {
+    @Test
+    void test_paragraph_contains_no_text_when_created_with_empty_constructor() {
+        Paragraph paragraph = new Paragraph();
+
+        assertEquals("<p></p>", this.output(paragraph));
+    }
+
+    @Test
+    void test_paragraph_contains_given_text_when_initialized_with_one() {
+        String content = "Content of a paragraph";
+        Paragraph paragraph = new Paragraph(content);
+
+        String output = this.output(paragraph);
+
+        assertTrue(output.contains(content));
+        assertTrue(output.contains("<p"));
+        assertTrue(output.contains("</p>"));
+    }
+
+    @Test
+    void test_paragraph_contains_given_text_when_content_set_afterwards() {
+        String content = "Content of a paragraph";
+        Paragraph paragraph = new Paragraph();
+        paragraph.setContent(content);
+
+        String output = this.output(paragraph);
+
+        assertTrue(output.contains(content));
+        assertTrue(output.contains("<p"));
+        assertTrue(output.contains("</p>"));
+    }
+}
