b80f7a3b1965e8d1e73b2b8a8a26a04c71eed0e7
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-11-27 22:31:21 +0100

    Bare minimum CSV reader

--- src/lab6/CSVReader.java
+++ src/lab6/CSVReader.java
@@ -1,5 +1,90 @@
 package lab6;
 
-public class CSVReader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class CSVReader {
+    public BufferedReader reader;
+    public String delimiter;
+    public boolean hasHeader;
+
+    // column names in the same order as in the file
+    public List<String> columnLabels = new ArrayList<>();
+
+    // map: column name -> column number
+    public Map<String, Integer> columnLabelsToInt = new HashMap<>();
+
+    public String[] current;
+
+    public CSVReader(String filename, String delimiter, boolean hasHeader) throws IOException {
+        this.reader = new BufferedReader(new FileReader(filename));
+        this.delimiter = delimiter;
+        this.hasHeader = hasHeader;
+
+        if (hasHeader) {
+            this.parseHeader();
+        }
+    }
+
+    void parseHeader() throws IOException {
+        String line = this.reader.readLine();
+
+        if (line == null) {
+            return;
+        }
+
+        String[] header = line.split(this.delimiter);
+
+        for (int i = 0; i < header.length; i++) {
+            this.columnLabelsToInt.put(header[i], i);
+            this.columnLabels.add(header[i]);
+        }
+    }
+
+    public boolean next() {
+        String line;
+
+        try {
+            line = this.reader.readLine();
+        } catch (IOException e) {
+            return false;
+        }
+
+        if (line == null) {
+            return false;
+        }
+
+        this.current = line.split(this.delimiter);
+
+        return true;
+    }
+
+    public String get(String columnName) {
+        return this.get(columnLabelsToInt.get(columnName));
+    }
+
+    public String get(int i) {
+        return this.current[i];
+    }
+
+    public int getInt(String columnName) {
+        return Integer.parseInt(this.get(columnName));
+    }
+
+    public int getInt(int i) {
+        return Integer.parseInt(this.get(i));
+    }
+
+    public double getDouble(String columnName) {
+        return Double.parseDouble(this.get(columnName));
+    }
+
+    public double getDouble(int i) {
+        return Double.parseDouble(this.get(i));
+    }
 }
---
+++ src/lab6/Main.java
@@ -0,0 +1,40 @@
+package lab6;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Main.withHeader();
+            System.out.println();
+            Main.withoutHeader();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+
+    private static void withHeader() throws IOException {
+        CSVReader reader = new CSVReader("/Users/Darek/Documents/Development/AGH/agh-java/src/lab6/assets/with-header.csv", ";", true);
+
+        while (reader.next()) {
+            int id = reader.getInt("id");
+            String surname = reader.get("nazwisko");
+            double home = reader.getDouble("nrdomu");
+
+            System.out.printf(Locale.US, "%d %s %f", id, surname, home);
+        }
+    }
+
+    private static void withoutHeader() throws IOException {
+        CSVReader reader = new CSVReader("/Users/Darek/Documents/Development/AGH/agh-java/src/lab6/assets/no-header.csv", ";", false);
+
+        while (reader.next()) {
+            int id = reader.getInt(0);
+            String surname = reader.get(2);
+            double home = reader.getDouble(4);
+
+            System.out.printf(Locale.US, "%d %s %f", id, surname, home);
+        }
+    }
+}
