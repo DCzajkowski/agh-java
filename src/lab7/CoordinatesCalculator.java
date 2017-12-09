5b4281d37270cc299bd9ad1c1608e72800c67c8b
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-12-09 03:50:10 +0100

    Use pow instead of duplicating

--- src/lab7/CoordinatesCalculator.java
+++ src/lab7/CoordinatesCalculator.java
@@ -10,9 +10,9 @@ public class CoordinatesCalculator {
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
 
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+        double a = Math.pow(Math.sin(latDistance / 2), 2)
             + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+            * Math.pow(Math.sin(lonDistance / 2), 2);
 
         return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) * 1000;
     }
